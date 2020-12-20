8d68624eeb7e335bf441589015455ddcbf93593d
philip <pkahly@gmail.com>
2020-12-19 22:29:03 -0500

    Add more props

--- Assets/Scripts/Rendering/WorldRenderer.cs
+++ Assets/Scripts/Rendering/WorldRenderer.cs
@@ -6,9 +6,17 @@ using UnityEngine.AI;
 
 public class WorldRenderer : MonoBehaviour
 {
+    private static readonly System.Random rand = new System.Random();
+
     // Prefabs
     public Transform wallPrefab = null;
     public Transform groundPrefab = null;
+    
+    // Props
+    public Transform[] rarePropPrefabs = null;
+    public Transform[] commonPropPrefabs = null;
+    [Range(0, 50)]
+    public int rarePropChance = 10;
 
     // Size of Maze Array
     [Range(10, 100)]
@@ -59,23 +67,55 @@ public class WorldRenderer : MonoBehaviour
                 int unityX = worldX * size;
                 int unityZ = worldZ * size;
 
-                // TODO: Create map from Type -> Prefab
                 if (space.type == WorldSpace.Type.ground) {
-                    DrawSpace(unityX, 0, unityZ, groundPrefab);
+                    DrawSpaceAndScale(unityX, 0, unityZ, groundPrefab);
+
+                    // Place rare prop
+                    if (rand.Next(rarePropChance) == 0) {
+                        PlaceRandomProp(unityX, unityZ, rarePropPrefabs);
+                    }
+
+                    // Place common props
+                    int numProps = rand.Next((size * size) / 2);
+                    for (int i = 0; i < numProps; i++) {
+                        PlaceRandomProp(unityX, unityZ, commonPropPrefabs);
+                    }
                 } else if (space.type == WorldSpace.Type.wall) {
-                    DrawSpace(unityX, 5, unityZ, wallPrefab);
+                    DrawSpaceAndScale(unityX, 5, unityZ, wallPrefab);
                 }
             }
         }
 
     }
 
-    private void DrawSpace(int unityX, int yOffset, int unityZ, Transform prefab) {
+    private void PlaceRandomProp(int unityX, int unityZ, Transform[] propPrefabs) {
+        if (propPrefabs != null && propPrefabs.Length > 0) {
+            // Pick random prop
+            var prop = propPrefabs[rand.Next(propPrefabs.Length)];
+
+            // Pick random place inside plane
+            int radius = size / 2;
+            int propX = rand.Next(unityX - radius, unityX + radius);
+            int propZ = rand.Next(unityZ - radius, unityZ + radius);
+
+            // Draw
+            DrawObj(propX, 0, propZ, prop);
+        }
+    }
+
+    private Transform DrawObj(int unityX, int yOffset, int unityZ, Transform prefab) {
         // Create new instance of prefab
         var obj = Instantiate(prefab, transform) as Transform;
 
         // Move to the given position, (in unity coordinates)
         obj.position = new Vector3(unityX, yOffset, unityZ);
+
+        return obj;
+    }
+
+    private void DrawSpaceAndScale(int unityX, int yOffset, int unityZ, Transform prefab) {
+        // Draw Prefab
+         var obj = DrawObj(unityX, yOffset, unityZ, prefab);
 
         // Scale the object to fill the spot
         // Multiply through by size because planes need their scale set to 0.1 instead of 1
